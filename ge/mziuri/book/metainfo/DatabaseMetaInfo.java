5f4e1cb64fd8cf45db3ffb951f30323ae88b2633
user <user>
2016-10-29 17:05:58 +0400

    initial commit...

---
+++ ge/mziuri/book/metainfo/DatabaseMetaInfo.java
@@ -0,0 +1,16 @@
+
+package ge.mziuri.book.metainfo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseMetaInfo {
+    
+    public static final String databaseURL = "jdbc:postgresql://localhost:5432/book";
+    
+    public static final String username = "postgres";
+    
+    public static final String passsword = "rame";
+    
+}
